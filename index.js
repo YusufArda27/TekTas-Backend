43ecc475ca726d9241ee80154a3d9dbd92d5d268
YusufArda27 <y.ardavardar@gmail.com>
2020-11-07 20:34:00 +0300

    Small Changes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const data=require('./data.json')
 let port = process.env.PORT || 8000;
 app.use(cors())
 
 app.get("/",(req,res)=>{
-    res.send("deneme")
+    res.send(data)
 })
 
 app.listen(port, ()=>{
